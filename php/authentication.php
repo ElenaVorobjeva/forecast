b2d598e423b9d247f7b09cea9441ee433aeda4fa
Elena Vorobjeva <vorobjeva556@yandex.ru>
2022-01-27 14:54:03 +0300

    v1.2

--- php/authentication.php
+++ php/authentication.php
@@ -2,31 +2,42 @@
 // ini_set('error_reporting', E_ALL);
 // ini_set('display_errors', 1);
 // ini_set('display_startup_errors', 1);
+
+/* 
+Проверка пользователя:
+1. Отправить запрос к БД
+2. Найти запись с введенным логином
+3. Взять id, логин и пароль пользователя
+4. Вернуть объект пользователя
+5. Сравнить данные с введенными (*)
+
+Если проверка успешная:
+1. Отправить запрос к БД 
+2. Найти пользователя по id
+3. Вернуть все оставшиеся данные
+4. Получить объект пользователя
+5. Получить список доступных для пользователя разделов
+6. Получить страницу по разделу
+7. Вернуть готовую страницу (*)
+
+Если проверка неуспешная:
+1. Вывести сообщение с тестом ошибки(*)
+*/
+
+include "get-user-data.php";
 include "get-one-workspace-page.php";
-
-$user = array(
-    "login" => "admin@yandex.ru",
-    "password" => "adminadmin" 
-);
-
-$sections = array("ЦФО", "ПФО", "СЗФО", "СФО", "ЮФО и СКФО", "УФО", "ДФО", "Беларусь", "Наукастинг", "Мониторинг", "Администрирование");
-
-$availableDistrictsId = array(0, 1, 2, 3, 4, 5, 6, 7);
-$priorityDistrictId = 3;
-$role = "admin";
-$isNewUser = false;
-
 
 $login = $_POST["emailInput"];
 $password = $_POST["passwordInput"];
+$user = getUserData($login);
 
-if($login == $user["login"]) {
+if($login != null) {
     if($password == $user["password"]) {
         //Вход успешный
         $response = array(
             "isOk" => true,
-            "content" => getOneWorkspacePage(getAvailableDistrictId($sections, $availableDistrictsId, $priorityDistrictId, $role), $isNewUser),
-            "title" => "Одна рабочая область"
+            "content" => getOneWorkspacePage($user),
+             "title" => "Одна рабочая область"
         );
     }
     else {
@@ -46,25 +57,4 @@ else {
 }
 
 echo json_encode($response);
-
-function getAvailableDistrictId($sections, $available, $priority, $role) {
-    // role
-    // admin - доступно администрирование
-    // moderator - доступны мониторинг, статистика, добавление статей
-    // user - доступны только регионы и наукастинг
-
-    array_splice($available, array_search($priority, $available), 1);
-    array_unshift($available, $priority);
-    if($role == "moderator" || $role == "admin") array_push($available, 9);
-    if($role == "admin") array_push($available, 10);
-    return getAvailableDistrict($sections, $available);
-}
-
-function getAvailableDistrict($sections, $available) {
-    for($i = 0; $i < count($available); $i++) {
-        $available[$i] = $sections[$available[$i]];
-    }
-    return $available;
-}
-
 ?>
